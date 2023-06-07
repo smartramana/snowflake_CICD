9e24e3dfefa4135bd4de47aad0504753f389e7b3
Syed Sha Khalid <95839964+8867156429@users.noreply.github.com>
2023-06-07 19:37:01 +0530

    Add files via upload

---
+++ migrations/V1.1.5__All_at_View.sql
@@ -0,0 +1,12 @@
+USE SCHEMA DEMO_ONE;
+CREATE VIEW All_In_VIEW
+(
+   FIRST_NAME VARCHAR, 
+   LAST_NAME VARCHAR, 
+   AGE NUMBER
+
+);
+
+INSERT INTO All_In_One
+VALUES ('MS', 'DHONI', 42), ('Rahul', 'Dravid', 52), ('Virat', 'Kholi', 38)
+;
