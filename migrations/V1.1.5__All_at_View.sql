20f21951ca9ab8f2a5609729c415e87253ffdbf1
Syed Sha Khalid <95839964+8867156429@users.noreply.github.com>
2023-06-07 19:39:24 +0530

    Update V1.1.5__All_at_View.sql

--- migrations/V1.1.5__All_at_View.sql
+++ migrations/V1.1.5__All_at_View.sql
@@ -1,5 +1,5 @@
 USE SCHEMA DEMO_ONE;
-CREATE VIEW All_In_VIEW
+CREATE VIEW All_In_one_VIEW
 (
    FIRST_NAME VARCHAR, 
    LAST_NAME VARCHAR, 
@@ -7,6 +7,6 @@ CREATE VIEW All_In_VIEW
 
 );
 
-INSERT INTO All_In_One
+INSERT INTO All_In_one_VIEW
 VALUES ('MS', 'DHONI', 42), ('Rahul', 'Dravid', 52), ('Virat', 'Kholi', 38)
 ;
