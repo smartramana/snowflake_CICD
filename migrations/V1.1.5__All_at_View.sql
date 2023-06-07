f4290e0ca17857bf738d9e2c9bbfb434d41797df
Syed Sha Khalid <95839964+8867156429@users.noreply.github.com>
2023-06-07 19:45:42 +0530

    Update V1.1.5__All_at_View.sql

--- migrations/V1.1.5__All_at_View.sql
+++ migrations/V1.1.5__All_at_View.sql
@@ -4,8 +4,4 @@ CREATE VIEW All_In_one_VIEW AS
 SELECT FIRST_NAME VARCHAR, 
        LAST_NAME VARCHAR, 
         AGE NUMBER
-FROM All_In_One
-
-INSERT INTO All_In_one_VIEW
-VALUES ('MS', 'DHONI', 42), ('Rahul', 'Dravid', 52), ('Virat', 'Kholi', 38)
-;
+FROM All_In_One ;
