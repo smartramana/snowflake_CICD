b43bcb8ee991facd08f8b89c33cc9fe381e808ad
Syed Sha Khalid <95839964+8867156429@users.noreply.github.com>
2023-06-07 19:25:16 +0530

    Add files via upload

---
+++ migrations/V1.1.4__All_at_once.sql
@@ -0,0 +1,14 @@
+CREATE SCHEMA DEMO_ONE;
+CREATE TABLE All_In_One
+(
+   FIRST_NAME VARCHAR, 
+   LAST_NAME VARCHAR, 
+   AGE NUMBER
+
+);
+
+INSERT INTO All_In_One
+VALUES ('MS', 'DHONI', 42),
+VALUES ('Rahul', 'Dravid', 52),
+VALUES ('Virat', 'Kholi', 38)
+;
